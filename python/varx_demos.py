e0ecc19d95ca1b4eab705217dfd334fb15b945cd
asortubay <asilvan000@citymail.cuny.edu>
2024-06-28 13:30:57 -0400

    fix relative paths for test data

--- python/varx_demos.py
+++ python/varx_demos.py
@@ -1,18 +1,20 @@
 # # test varx (without basis)
 import varxModel
 import scipy.io as sio
-x = sio.loadmat('testdata/x.mat')['x']
-y = sio.loadmat('testdata/y.mat')['y']
+x = sio.loadmat('./testdata/x.mat')['x']
+y = sio.loadmat('./testdata/y.mat')['y']
 L = 10
 na = 10; nb = 20; gamma = 0
 
 model = varxModel.varx(y,na,x,nb,gamma)
+print(model)
 
 # # test varx (with basis)
-x = sio.loadmat('x_basis.mat')['x']
-y = sio.loadmat('y_basis.mat')['y']
+x = sio.loadmat('./testdata/x_basis.mat')['x']
+y = sio.loadmat('./testdata/y_basis.mat')['y']
 L = 10
 na = 3; nb = 4; gamma = 0.1
-base = basis(30, nb, 'normal')
+base = varxModel.basis(30, nb, 'normal')
 model = varxModel.varx(y,na,x,base,gamma)
     
+print(model)
